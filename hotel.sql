ca1618533e56d93c2b47adf6b0aee4987beb2925
nureensyafiqah <107804361+nureensyafiqah@users.noreply.github.com>
2023-07-01 23:58:58 +0800

    Add files via upload

---
+++ hotel.sql
@@ -0,0 +1,230 @@
+-- phpMyAdmin SQL Dump
+-- version 4.2.7.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 14, 2017 at 06:52 PM
+-- Server version: 5.6.20
+-- PHP Version: 5.5.15
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `hotel`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contact`
+--
+
+CREATE TABLE IF NOT EXISTS `contact` (
+`id` int(10) unsigned NOT NULL,
+  `fullname` varchar(100) DEFAULT NULL,
+  `phoneno` int(10) DEFAULT NULL,
+  `email` text,
+  `cdate` date DEFAULT NULL,
+  `approval` varchar(12) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login`
+--
+
+CREATE TABLE IF NOT EXISTS `login` (
+`id` int(10) unsigned NOT NULL,
+  `usname` varchar(30) DEFAULT NULL,
+  `pass` varchar(30) DEFAULT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `login`
+--
+
+INSERT INTO `login` (`id`, `usname`, `pass`) VALUES
+(1, 'Admin', '1234'),
+(2, 'Prasath', '12345');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `newsletterlog`
+--
+
+CREATE TABLE IF NOT EXISTS `newsletterlog` (
+`id` int(10) unsigned NOT NULL,
+  `title` varchar(52) DEFAULT NULL,
+  `subject` varchar(100) DEFAULT NULL,
+  `news` text
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payment`
+--
+
+CREATE TABLE IF NOT EXISTS `payment` (
+  `id` int(11) DEFAULT NULL,
+  `title` varchar(5) DEFAULT NULL,
+  `fname` varchar(30) DEFAULT NULL,
+  `lname` varchar(30) DEFAULT NULL,
+  `troom` varchar(30) DEFAULT NULL,
+  `tbed` varchar(30) DEFAULT NULL,
+  `nroom` int(11) DEFAULT NULL,
+  `cin` date DEFAULT NULL,
+  `cout` date DEFAULT NULL,
+  `ttot` double(8,2) DEFAULT NULL,
+  `fintot` double(8,2) DEFAULT NULL,
+  `mepr` double(8,2) DEFAULT NULL,
+  `meal` varchar(30) DEFAULT NULL,
+  `btot` double(8,2) DEFAULT NULL,
+  `noofdays` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `room`
+--
+
+CREATE TABLE IF NOT EXISTS `room` (
+`id` int(10) unsigned NOT NULL,
+  `type` varchar(15) DEFAULT NULL,
+  `bedding` varchar(10) DEFAULT NULL,
+  `place` varchar(10) DEFAULT NULL,
+  `cusid` int(11) DEFAULT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=16 ;
+
+--
+-- Dumping data for table `room`
+--
+
+INSERT INTO `room` (`id`, `type`, `bedding`, `place`, `cusid`) VALUES
+(1, 'Damai Suite', 'King', 'Free', NULL),
+(2, 'Damai Suite', 'King', 'Free', NULL),
+(3, 'Damai Suite', 'Queen', 'Free', NULL),
+(4, 'Cahaya Suite', 'King', 'Free', NULL),
+(5, 'Damai Suite', 'Queen', 'Free', NULL),
+(6, 'Bayu Suria Suite', 'King', 'Free', NULL),
+(7, 'Bayu Suria Suite', 'Queen', 'Free', NULL),
+(8, 'Bayu Suria Suite', 'King', 'Free', NULL),
+(9, 'Bayu Suria Suite', 'Queen', 'Free', NULL),
+(10, 'Bayu Senja Suite', 'Queen', 'Free', NULL),
+(11, 'Bayu Senja Suite', 'Queen', 'Free', NULL),
+(12, 'Bayu Senja Suite', 'King', 'Free', NULL),
+(13, 'Cahaya Suite', 'Queen', 'Free', NULL),
+(14, 'Cahaya Suite', 'Queen', 'Free', NULL),
+(15, 'Cahaya Suite', 'King', 'Free', NULL),
+(16, 'Bayu Senja Suite', 'King', 'Free', NULL),
+(17, 'Bayu Family Suite', 'Queen', 'Free', NULL),
+(18, 'Bayu Family Suite', 'Queen', 'Free', NULL),
+(19, 'Anjung Suite', 'Queen', 'Free', NULL),
+(20, 'Bayu Family Suite', 'King', 'Free', NULL),
+(21, 'Anjung Suite', 'Queen', 'Free', NULL),
+(22, 'Anjung Suite', 'King', 'Free', NULL),
+(23, 'Anjung Suite', 'King', 'Free', NULL),
+(24, 'Bayu Family Suite', 'King', 'Free', NULL);
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `roombook`
+--
+
+CREATE TABLE IF NOT EXISTS `roombook` (
+`id` int(10) unsigned NOT NULL,
+  `Title` varchar(5) DEFAULT NULL,
+  `FName` text,
+  `LName` text,
+  `Email` varchar(50) DEFAULT NULL,
+  `National` varchar(30) DEFAULT NULL,
+  `Country` varchar(30) DEFAULT NULL,
+  `Phone` text,
+  `TRoom` varchar(20) DEFAULT NULL,
+  `Bed` varchar(10) DEFAULT NULL,
+  `NRoom` varchar(2) DEFAULT NULL,
+  `Meal` varchar(15) DEFAULT NULL,
+  `cin` date DEFAULT NULL,
+  `cout` date DEFAULT NULL,
+  `stat` varchar(15) DEFAULT NULL,
+  `nodays` int(11) DEFAULT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `contact`
+--
+ALTER TABLE `contact`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `login`
+--
+ALTER TABLE `login`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `newsletterlog`
+--
+ALTER TABLE `newsletterlog`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `room`
+--
+ALTER TABLE `room`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `roombook`
+--
+ALTER TABLE `roombook`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `contact`
+--
+ALTER TABLE `contact`
+MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `login`
+--
+ALTER TABLE `login`
+MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `newsletterlog`
+--
+ALTER TABLE `newsletterlog`
+MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `room`
+--
+ALTER TABLE `room`
+MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=16;
+--
+-- AUTO_INCREMENT for table `roombook`
+--
+ALTER TABLE `roombook`
+MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ room.php
@@ -0,0 +1,148 @@
+<?php
+include('db.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>TANJUNG RHU RESORT</title>
+<!-- for-mobile-apps -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Resort Inn Responsive , Smartphone Compatible web template , Samsung, LG, Sony Ericsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+		function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!-- //for-mobile-apps -->
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+<link href="css/font-awesome.css" rel="stylesheet"> 
+<link rel="stylesheet" href="css/chocolat.css" type="text/css" media="screen">
+<link href="css/easy-responsive-tabs.css" rel='stylesheet' type='text/css'/>
+<link rel="stylesheet" href="css/flexslider.css" type="text/css" media="screen" property="" />
+<link rel="stylesheet" href="css/jquery-ui.css" />
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+<script type="text/javascript" src="js/modernizr-2.6.2.min.js"></script>
+<!--fonts-->
+<link href="//fonts.googleapis.com/css?family=Oswald:300,400,700" rel="stylesheet">
+<link href="//fonts.googleapis.com/css?family=Federo" rel="stylesheet">
+<link href="//fonts.googleapis.com/css?family=Lato:300,400,700,900" rel="stylesheet">
+<!--//fonts-->
+</head>
+<body>
+<div class="plans-section" id="rooms">
+	 <div class="container">
+	 <h3 class="title-w3-agileits title-black-wthree">ROOMS AND RATES</h3>
+	 <div class="price-selet">
+	 <a href="reservation.php" >BOOK NOW!</a>
+</div>
+		<div class="priceing-table-main">
+			<div class="col-md-4 price-grid">
+					<div class="price-block agile">
+						<div class="price-gd-top">
+						<img src="images/damai.jpg" alt=" " class="img-responsive" />
+							<h4>DAMAI SUITE</h4>
+						</div>
+						<div class="price-gd-bottom">
+							   <div class="price-list">
+							<h5>   GARDEN/COURTYARD VIEW</h5>
+							<h6>Room size: 50 sqm. The Damai Suites have a living area and a small balcony with views of the central garden courtyard.</h6>
+							</div>
+							<div class="price-selet">	
+								<h3><span>RM</span>320</h3>						
+
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4 price-grid ">
+					<div class="price-block agile">
+						<div class="price-gd-top">
+						<img src="images/cahaya.jpg" alt=" " class="img-responsive" />
+							<h4>CAHAYA SUITE</h4>
+						</div>
+						<div class="price-gd-bottom">
+							<div class="price-list">
+									<h5>SEA VIEW</h5>
+                                    <h6>A 100 sqm guestroom at our accommodation in Langkawi with a view of the sea, pool, 
+										and garden in this 5 Star Langkawi Resort.</h6>
+							</div>
+							<div class="price-selet">
+								<h3><span>RM</span>220</h3>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4 price-grid lost">
+					<div class="price-block agile">
+						<div class="price-gd-top">
+						<img src="images/bayusuria.jpg" alt=" " class="img-responsive" />
+							<h4>BAYU SURIA SUITE</h4>
+						</div>
+						<div class="price-gd-bottom">
+							<div class="price-list">
+								<h5>POOL/GARDEN VIEW</h5>
+								<h6>If you seek a little more space and the added luxury of a separate living area and 2 balconies, 
+									the Bayu Suria Pool/Garden View Room is an ideal choice.</h6>
+							</div>
+							<div class="price-selet">
+								<h3><span>RM</span>180</h3>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4 price-grid wthree lost">
+					<div class="price-block agile">
+						<div class="price-gd-top ">
+							<img src="images/bayusenja.jpg" alt=" " class="img-responsive" />
+							<h4>BAYU SENJA SUITE</h4>
+						</div>
+						<div class="price-gd-bottom">
+							<div class="price-list">
+								<h5>SEA VIEW</h5>
+								<h6>The Bayu Senja Suite is a spacious unit with a bedroom that has oversized windows with stunning 
+									views of the sea in one of the top Langkawi Luxury resorts.</h6>
+							</div>
+							<div class="price-selet">
+								<h3><span>RM</span> 150</h3>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4 price-grid wthree lost">
+					<div class="price-block agile">
+						<div class="price-gd-top ">
+							<img src="images/bayufam.jpg" alt=" " class="img-responsive" />
+							<h4>BAYU FAMILY SUITE</h4>
+						</div>
+						<div class="price-gd-bottom">
+							<div class="price-list">
+								<h5>GARDEN VIEW</h5>
+								<h6>This family room has a balcony, air conditioning and satellite TV.</h6>
+							</div>
+							<div class="price-selet">
+								<h3><span>RM</span> 650</h3>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4 price-grid wthree lost">
+					<div class="price-block agile">
+						<div class="price-gd-top ">
+							<img src="images/anjung.jpg" alt=" " class="img-responsive" />
+							<h4>ANJUNG SUITE</h4>
+						</div>
+						<div class="price-gd-bottom">
+							<div class="price-list">
+							<h6>A 100 sqm guestroom at our accommodation in Langkawi with a view of the sea, pool, 
+										and garden in this 5 Star Langkawi Resort.</h6>
+							</div>
+							<div class="price-selet">
+								<h3><span>RM</span> 750</h3>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
